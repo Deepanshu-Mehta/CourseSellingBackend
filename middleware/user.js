6553ab16015d0e5f89071e07e73c586e5c22b765
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-11-06 02:58:25 +0530

    updated

--- middleware/user.js
+++ middleware/user.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 function userMiddleware(req, res, next) {
     // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
     const token = headers.authentication;
     const jwtToken = token.split(" ")[1];
     const decoded = jwt.verify(jwtToken, process.env.JWT_SECERET);
